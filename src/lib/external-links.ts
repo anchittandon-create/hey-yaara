f70d050d084eee3864d09a86a283106d9c5fa949
anchittandon-create <anchit.tandon@gmail.com>
2026-04-07 01:08:28 +0530

    h

--- src/lib/external-links.ts
+++ src/lib/external-links.ts
@@ -1,11 +1,4 @@
 export const openExternalUrlInNewTab = (url: string) => {
-  const newTab = window.open("", "_blank", "noopener,noreferrer");
-
-  if (!newTab) {
-    return false;
-  }
-
-  newTab.opener = null;
-  newTab.location.href = url;
-  return true;
+  const newTab = window.open(url, "_blank", "noopener,noreferrer");
+  return Boolean(newTab);
 };
